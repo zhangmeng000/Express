ae0c6b7f121496ce138c52958465c1dfe9479505
zhangmeng000 <1550685677@qq.com>
2016-09-27 16:22:52 +0800

    dd

---
+++ index.js
@@ -0,0 +1,15 @@
+var express = require('express')
+var app = express()
+
+app.get('/:name', function (req,res) {
+	var username = req.params.name;
+	var page = "<html>" + "<body>" + "<h2>" + username + "的购物车" + "</h2>" + "</body>" + "</html>"
+  res.send(page)
+})
+app.get('/aaa', function (req, res) {
+	var page = "<html>" + "<body>" + "<h2>aaa.html</h2>" + "</body>" + "</html>"
+  console.log('hello aaa')
+})
+app.listen(3000,function(){
+	console.log('running on port 3000...please visit localhost:3000')
+})
